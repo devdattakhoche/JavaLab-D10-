956a2a6846c619879a05b064fe98ceaf509c22fc
devdattakhoche <49261633+devdattakhoche@users.noreply.github.com>
2019-11-03 15:15:41 +0530

    Palindrome

---
+++ palindrome.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class plaindrome{
+	public static void main(String args[])
+	{
+		String original,reverse="";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number");
+		original=sc.nextLine();
+		int length = original.length();
+		for(int i=length-1;i>=0;i--)
+		{
+			reverse=reverse+original.charAt(i);
+		}
+		boolean temp=false;
+		System.out.println(original);
+		System.out.println(reverse);
+		/*for(int i=0;i<length;i++)
+		{
+		   if(original.charAt(i)==reverse.charAt(length-1-i))
+		   {
+			   temp=true;
+		   }
+		   else 
+		   {
+			   temp=false;
+			   break;
+		   }
+		}
+		System.out.println(temp);*/
+		 if (original.equals(reverse))  
+         System.out.println("Entered string/number is a palindrome.");  
+      else  
+         System.out.println("Entered string/number isn't a palindrome.");   
+		
+	}
+}
